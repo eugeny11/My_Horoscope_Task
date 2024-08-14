eb91ecb547e8806ae85fda436683570ec96e1a27
eugeny11 <eugeny11@mail.ru>
2024-08-14 15:12:39 +0200

    Updated the API testing

--- api/telegram.js
+++ api/telegram.js
@@ -3,28 +3,27 @@ import axios from "axios";
 export default async function handler(req, res) {
   console.log("Webhook triggered");
   if (req.method === "POST") {
-    const { message } = req.body;
+    try {
+      const { message } = req.body;
+      if (message && message.text === "/start") {
+        console.log("Start command received");
+        const chatId = message.chat.id;
+        const token = process.env.TELEGRAM_BOT_TOKEN;
+        const url = "https://my-horoscope-task.vercel.app";
 
-    if (message && message.text === "/start") {
-      console.log("Start command received");
-      const chatId = message.chat.id;
-      const token = process.env.TELEGRAM_BOT_TOKEN;
-      const url = "https://my-horoscope-task.vercel.app";
-
-      const replyMarkup = {
-        reply_markup: {
-          inline_keyboard: [
-            [
-              {
-                text: "Open Horoscope App",
-                web_app: { url },
-              },
+        const replyMarkup = {
+          reply_markup: {
+            inline_keyboard: [
+              [
+                {
+                  text: "Open Horoscope App",
+                  web_app: { url },
+                },
+              ],
             ],
-          ],
-        },
-      };
+          },
+        };
 
-      try {
         const response = await axios.post(
           `https://api.telegram.org/bot${token}/sendMessage`,
           {
@@ -33,19 +32,21 @@ export default async function handler(req, res) {
             ...replyMarkup,
           }
         );
+        console.log("Message sent successfully:", response.data);
         res.status(200).json(response.data);
-      } catch (error) {
-        console.error(
-          "Error sending message:",
-          error.response?.data || error.message
-        );
-        res.status(500).send("Failed to send message");
+      } else {
+        console.log("Message not recognized");
+        res.status(200).send("No action needed");
       }
-    } else {
-      console.log("Message not recognized");
-      res.status(200).send("No action needed");
+    } catch (error) {
+      console.error(
+        "Error processing request:",
+        error.response?.data || error.message
+      );
+      res.status(500).send("Failed to process request");
     }
   } else {
+    console.log("Wrong request method");
     res.status(405).send("Method not allowed");
   }
 }
